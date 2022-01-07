55bf004daad6f0c000e94329bbf04e8339379357
Nabyeol An <an1413@hs.ac.kr>
2022-01-07 19:21:34 +0900

    java_tutorials 220107
    문자열 스트링

---
+++ java01/src/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		System.out.println('积');
+		System.out.println('积');
+    System.out.println("积劝内爹");
+	}
+	
+}
